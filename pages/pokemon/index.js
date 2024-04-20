0130aa7ecdf725aea1f0e3a2f1f4d13babb93222
roncon06 <107638770+roncon06@users.noreply.github.com>
2024-04-20 16:45:18 +0000

    Atividade 18/04

--- pages/pokemon/index.js
+++ pages/pokemon/index.js
@@ -1,81 +1,68 @@
 function changePageTitle(title) {
-  document.title = title;
+  document.title = title
 }
 
 function generateInfoSection(sprites, pokemonName) {
-  const h2 = document.createElement('h2');
-  h2.id = "info-pokemon-label";
-  h2.textContent = `Informações sobre ${pokemonName}`;
+  const imagens = Object.values(sprites)
+    .filter(sprite => typeof sprite === 'string')
 
-  const img = document.querySelector('img');
-  img.src = sprites[0];
-  img.alt = `Imagem do pokemon ${pokemonName}`;
-  let currentIndex = 0;
+  const h2 = document.createElement('h2')
+  h2.id = "info-pokemon-label"
+  h2.textContent = `Informações sobre ${pokemonName}`
+
+  const img = document.querySelector('img')
+  img.src = imagens[0]
+  img.alt = `Imagem do pokemon ${pokemonName}`
+
+  const section = document.querySelector('#info-pokemon')
+
+  section.appendChild(h2)
+  section.appendChild(img)
+
+  let indiceAtual = 0;
 
   img.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % sprites.length;
-    img.src = sprites[currentIndex];
+    indiceAtual = (indiceAtual + 1) % imagens.length;
+    img.src = imagens[indiceAtual];
   });
-
-  const section = document.querySelector('#info-pokemon');
-  section.appendChild(h2);
-  section.appendChild(img);
 }
 
-async function getPokemonData(name) {
-  try {
-    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    const jsonData = await data.json();
 
-    const spritesArray = Object.values(jsonData.sprites).filter(sprite => typeof sprite === 'string');
-    generateInfoSection(spritesArray, name);
+async function getPokemonData(name) {
+  // fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+  //   .then((fetchData) => {
+  //     return fetchData.json()
+  //   })
+  //   .then((jsonData) => generateInfoSection(jsonData.sprites.front_default, name))
+  //   .catch((error) => console.error(error))
+
+  try {
+    const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+
+    const jsonData = await data.json()
+
+    generateInfoSection(jsonData.sprites, name)
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 }
 
 function getSearchParams() {
-  
+  // Early return -> Caso location search, não faz nada.
   if (!location.search) {
-    return;
-  }
-  
-  const urlSearchParams = new URLSearchParams(location.search);
-  
-  
-  const pokemonName = urlSearchParams.get('name');
-  
-  changePageTitle(`Pagina do ${pokemonName}`);
-  getPokemonData(pokemonName);
-}
-
-function updateCounter() {
-  const storageKey = 'pageVisitCount';
-  const now = new Date();
-  const formattedDate = new Intl.DateTimeFormat('pt-BR', {
-    year: 'numeric', month: '2-digit', day: '2-digit',
-    hour: '2-digit', minute: '2-digit'
-  }).format(now);
-
-  let visitData = localStorage.getItem(storageKey);
-  if (visitData) {
-    visitData = JSON.parse(visitData);
-    visitData.count++;
-    visitData.lastVisit = formattedDate;
-  } else {
-    visitData = { count: 1, lastVisit: formattedDate };
+    return
   }
 
-  localStorage.setItem(storageKey, JSON.stringify(visitData));
+  // URLSearchParams é uma classe que facilita a manipulação de query strings
+  const urlSearchParams = new URLSearchParams(location.search)
 
-  const footer = document.querySelector('footer');
-  const visitInfoParagraph = document.createElement('p');
-  visitInfoParagraph.textContent = `Esta página foi visitada ${visitData.count} vezes. A última visita foi: ${visitData.lastVisit}`;
+  // Pegando o valor do parâmetro name
+  const pokemonName = urlSearchParams.get('name')
 
-
-  footer.appendChild(visitInfoParagraph);
+  changePageTitle(`Pagina do ${pokemonName}`)
+  getPokemonData(pokemonName)
 }
+
 document.addEventListener('DOMContentLoaded', function () {
-  getSearchParams();
-  updateCounter();
-});
+  getSearchParams()
+})
